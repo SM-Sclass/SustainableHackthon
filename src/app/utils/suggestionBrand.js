82b07a832aa66bf1d2991870b2d7fc9c5b1ea076
Harish Sargar <harishsargar19@gmail.com>
2024-11-10 02:25:34 +0530

    harish changes

--- src/app/utils/suggestionBrand.js
+++ src/app/utils/suggestionBrand.js
@@ -29,62 +29,8 @@ export const suggestionBrand = [
         ],
 
     },
-    
-    // {
-    //     "id": 2,
-    //     "categoryImg": "/category/Suscloth.png",
-    //     "categoryName": "Food",
-    //     "categoryRoute": "/clothing",
-    //     "categoryBrands": [
-    //         {
-    //             "id":"FD1",
-    //             "image": "/BrandDisplay/NoNasties.png",
-    //             "brandName": "No Nasties"
-    //         },
-    //         {
-    //             "id":"FD2",
-    //             "image": "/BrandDisplay/TerraTribe.png",
-    //             "brandName": "The Terra Tribe"
-    //         },
-    //         {
-    //             "id":"FD3",
-    //             "image": "/BrandDisplay/BigStory.jpeg",
-    //             "brandName": "A Big Indian Story"
-    //         },
-    //         {
-    //             "id":"FD4",
-    //             "image": "/BrandDisplay/theSummerhouse.png",
-    //             "brandName": "The Summer House"
-    //         }
-    //     ],
-    // }
+
     {
-<<<<<<< HEAD
-        "id": 3,
-        "categoryImg": "/category/SusCosmetics.png",
-        "categoryName": "Cosmetics",
-        "categoryRoute": "/cosmetics",
-        "categoryBrands": [
-            {
-                "id":"CS1",
-                "image": "/BrandDisplay/ForestEssentials.png",
-                "brandName": "Forest Essentials"
-            },
-            {
-                "id":"CS2",
-                "image": "/BrandDisplay/NaturalEssence.png",
-                "brandName": "Natural Essence"
-            },
-            {
-                "id":"CS3",
-                "image": "/BrandDisplay/JuicyChemisrty.png",
-                "brandName": "Juicy Chemistry"
-            },
-            {
-                "id":"CS4",
-                "image": "/BrandDisplay/RawNature.png",
-                "brandName": "Raw Nature"
-=======
         "id": 2,
         "categoryImg": "/category/foood.jpg",
         "categoryName": "Food",
@@ -109,7 +55,35 @@ export const suggestionBrand = [
                 "id":"FD4",
                 "image": "/BrandDisplay/ZeroCowFactory.png",
                 "brandName": "Zero Cow Factory"
->>>>>>> b57f5bbfd03ae054b74eb9094b78a089d0688eb2
+
+            }
+        ],
+    },
+    {
+        "id": 3,
+        "categoryImg": "/category/SusCosmetics.png",
+        "categoryName": "Cosmetics",
+        "categoryRoute": "/cosmetics",
+        "categoryBrands": [
+            {
+                "id":"CS1",
+                "image": "/BrandDisplay/ForestEssentials.png",
+                "brandName": "Forest Essentials"
+            },
+            {
+                "id":"CS2",
+                "image": "/BrandDisplay/NaturalEssence.png",
+                "brandName": "Natural Essence"
+            },
+            {
+                "id":"CS3",
+                "image": "/BrandDisplay/JuicyChemisrty.png",
+                "brandName": "Juicy Chemistry"
+            },
+            {
+                "id":"CS4",
+                "image": "/BrandDisplay/RawNature.png",
+                "brandName": "Raw Nature"
             }
         ],
     }
